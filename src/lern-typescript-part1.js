8711164ab15da37dc3e752eead1ef7f3733f7c01
muhammedarshadtp <arshadtp89@gmail.com>
2025-06-10 23:10:41 +0530

    TS all topic comleted

--- src/lern-typescript-part1.js
+++ src/lern-typescript-part1.js
@@ -37,3 +37,79 @@ var sum = function (p, d) { return p + d; };
 var mult = function (p, d) { return p * d; };
 console.log(sum(2, 3));
 console.log(mult(2, 3));
+var milan = {
+    name: "milan",
+    address: "germeny"
+};
+console.log(milan);
+// Type Narrowing
+function getPosition(postion) {
+    if (typeof postion === "string")
+        console.log(postion.split(" "));
+    else
+        console.log(postion.toFixed());
+}
+getPosition(22);
+getPosition("Arshad");
+function checkAndPrint(value) {
+    if (value && typeof value === "object") {
+        for (var _i = 0, value_1 = value; _i < value_1.length; _i++) {
+            var key = value_1[_i];
+            console.log(key);
+        }
+    }
+    else if (value === "string") {
+        console.log(value.toLocaleLowerCase());
+    }
+}
+checkAndPrint(['cat', 'dog', 'mouse']);
+checkAndPrint('Arshad');
+// Narrowing using "Equaily" operater
+function equalChecking(a, b) {
+    if (a === b) {
+        a.split(' ');
+        b.toLocaleUpperCase();
+    }
+    else {
+        console.log(a);
+        console.log(b);
+    }
+}
+function activity(actor) {
+    if ("study" in actor) {
+        return actor.study();
+    }
+    else {
+        return actor.work();
+    }
+}
+// Narrowing using "instanceof" checking
+function Checking(value) {
+    if (value instanceof Date) {
+        console.log(value.getTime());
+    }
+    else {
+        console.log(value.toLocaleLowerCase());
+    }
+}
+// Narrowing using "Type Predicates"
+/*
+this is return a boolen value
+they have a perticular return type we using key ward is called " is "
+*/
+function isNumber(value) {
+    return typeof value === 'number';
+}
+console.log(isNumber(3));
+var s1 = { kind: 'cricle', radius: 30 };
+var s2 = { kind: 'squre', side: 28 };
+function getArea(s) {
+    if (s.kind === 'squre') {
+        return s.side * s.side;
+    }
+    else {
+        return Math.PI * s.radius * s.radius;
+    }
+}
+console.log(getArea(s1));
+console.log(getArea(s2));
--- src/lern-typescript-part1.ts
+++ src/lern-typescript-part1.ts
@@ -82,3 +82,125 @@ console.log(mult(2,3))
 
 
 // Interfaces
+
+
+interface ICustmer {
+    name:string
+    address : string
+}
+
+const milan:ICustmer ={
+    name:"milan",
+    address:"germeny"
+}
+
+console.log(milan)
+
+
+// Type Narrowing
+
+
+function getPosition(postion:string| number){
+
+    if(typeof postion === "string") console.log(postion.split(" "))
+        else console.log(postion.toFixed())
+
+}
+
+getPosition(22)
+getPosition("Arshad")
+
+
+function checkAndPrint(value:string|string[]|null){
+    if( value && typeof value === "object"){
+        for(const key of value){
+            console.log(key)
+        }
+    }else if(value === "string"){
+        console.log(value.toLocaleLowerCase())
+    }
+}
+
+checkAndPrint(['cat','dog','mouse'])
+checkAndPrint('Arshad')
+
+// Narrowing using "Equaily" operater
+
+function equalChecking(a:string|number,b:string|string[]){
+    if(a===b){
+        a.split(' ')
+        b.toLocaleUpperCase()
+    }else{
+        console.log(a)
+        console.log(b)
+    }
+}
+
+// Narrowing using "in" operater
+
+
+ type Student= {study :  () => {} }
+ type Elpy = {work : () => {}}
+
+ function activity(actor:Student|Elpy){
+    if("study" in actor){
+        return actor.study()
+    }else{
+        return actor.work()
+    }
+ }
+
+
+ // Narrowing using "instanceof" checking
+
+  function Checking(value:string|Date){
+    if( value instanceof Date){
+        console.log(value.getTime())
+    }else{
+        console.log(value.toLocaleLowerCase( ))
+    }
+  }
+
+
+  // Narrowing using "Type Predicates"
+
+   /* 
+   this is return a boolen value
+   they have a perticular return type we using key ward is called " is "
+   */
+
+   function isNumber(value:any): value is number {
+    return typeof value === 'number'
+   }
+
+   console.log(isNumber(3))
+
+   // Narrowing using "discriminated unions"
+
+   type cricle = {
+    kind:"cricle"
+    radius : number
+   }
+
+   type squre  ={
+    kind:"squre"
+    side : number
+   }
+
+   type shape = cricle | squre
+
+   const s1 : shape = {kind:'cricle',radius:30};
+
+   const s2 : shape = {kind:'squre', side:28}
+
+   function getArea(s:shape){
+    if(s.kind === 'squre'){
+        return s.side * s.side
+    }else{
+        return Math.PI * s.radius*s.radius
+    }
+   }
+
+   console.log( getArea(s1))
+  console.log( getArea(s2))
+   
